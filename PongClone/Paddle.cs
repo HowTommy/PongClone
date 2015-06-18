f51519c2a9b85d3e35410e245eff1bf2f00f37ec
HowTommy <howtommy.net@mail.com>
2015-06-19 00:36:23 +0200

    First version

---
+++ PongClone/Paddle.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PongClone
+{
+    public class Paddle : Sprite
+    {
+        private Rectangle _screenBounds;
+
+        public Paddle(Texture2D texture, Vector2 vector, Rectangle screenBounds)
+            : base(texture, vector)
+        {
+            _screenBounds = screenBounds;
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            if (Keyboard.GetState().IsKeyDown(Keys.Up))
+            {
+                _velocity = new Vector2(0, -5f);
+            }
+            else if (Keyboard.GetState().IsKeyDown(Keys.Down))
+            {
+                _velocity = new Vector2(0, 5f);
+            }
+            else
+            {
+                _velocity = new Vector2(0, 0);
+            }
+
+            base.Update(gameTime);
+        }
+
+        protected override void CheckBounds()
+        {
+            _location.Y = MathHelper.Clamp(_location.Y, 0, _screenBounds.Height - _texture.Height);
+        }
+    }
+}
---
+++ PongClone/Sprite.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PongClone
+{
+    public abstract class Sprite
+    {
+        protected Texture2D _texture;
+        protected Vector2 _location;
+        protected Vector2 _velocity = Vector2.Zero;
+
+        public Sprite(Texture2D texture, Vector2 location)
+        {
+            this._texture = texture;
+            this._location = location;
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(_texture, _location, Color.White);
+        }
+
+        public virtual void Update(GameTime gameTime)
+        {
+            _location += _velocity;
+
+            CheckBounds();
+        }
+
+        protected abstract void CheckBounds();
+    }
+}
